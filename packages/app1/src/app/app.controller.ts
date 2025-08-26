c66584276ea79eeb6ef2cdaffbc07e9c6ebe34fc
stepbirt <stepbirt@gmail.com>
2025-08-26 20:27:08 +0700

    fix: try to set http proxy

--- packages/app1/src/app/app.controller.ts
+++ packages/app1/src/app/app.controller.ts
@@ -1,12 +1,33 @@
+import { AxiosError } from './../../../../node_modules/axios/index.d';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
+import { catchError, map } from 'rxjs';
+import { error } from 'console';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService
+  ) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('test')
+  async test() {
+    const test = await this.httpService
+      .get('https://api.ipify.org/?format=json')
+      .pipe(
+        map((response) => response.data),
+        catchError((error: AxiosError) => {
+          console.log(error.message);
+          throw error;
+        })
+      );
+    return test;
   }
 }
--- packages/app1/src/app/app.module.ts
+++ packages/app1/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpModule } from '@nestjs/axios';
+import { ProxyAgent } from 'proxy-agent';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule.register({
+      httpsAgent: new ProxyAgent(), // Handles HTTPS via proxy, with cert bypass
+      proxy: false, // must disable axios's own proxy logic
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
